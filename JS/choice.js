27aa0221e1a8ecaf0c46e4e7a7cef49c0885b1da
guidephy <50879237+guidephy@users.noreply.github.com>
2022-09-15 23:27:20 +0800

    Add files via upload

---
+++ JS/choice.js
@@ -0,0 +1,19 @@
+
+
+$('.js-start').on('click', function () {
+
+function getRandom(x){
+    return Math.floor(Math.random()*x)+1;
+};
+
+var x=getRandom(39);
+
+x=1+x*3;
+
+
+    $('.js-result').text('等待結果...');
+    setTimeout(function() {
+
+        $('.js-result').text('抽到的人是：' + gradedata[x]);
+    }, 2000);
+});
