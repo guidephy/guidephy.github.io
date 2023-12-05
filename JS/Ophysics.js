174809122004034d052cf0caaa35fd5f899b331d
guidephy <50879237+guidephy@users.noreply.github.com>
2023-12-06 06:34:34 +0800

    Add files via upload

--- JS/Ophysics.js
+++ JS/Ophysics.js
@@ -1,12 +1,33 @@
-questionAppearTime=5*1000;
-    // 影片結束後顯示問題和按鈕
+    var parameter = {};
+    var questiondata;
+    var youtubeEmbedUrl;
+    var iframe = document.getElementById('vedioUrl');
+    var n=1;
+
+    // 使用 jQuery 的 $.get 方法發送 GET 請求
+    $.get("https://script.google.com/macros/s/AKfycbyBbvR9w9_wdQlOVi9h9P8u8iUs0BKNlsOGzZDR3muqRW4yWSKrKhsdB43v4nwaVDpL/exec", parameter, function(data) {
+      questiondata = data.split(",");
+      alert(questiondata[10]);
+     
+
+    youtubeEmbedUrl = 'https://www.youtube.com/embed/' + questiondata[10];
+      // 更改 <iframe> 的 src 屬性
+     iframe.src = youtubeEmbedUrl;
+
+      // 影片結束後顯示問題和按鈕
+        questionAppearTime=questiondata[11]*1000;
+       setTimeout(function() {
+            document.getElementById("Question").style.visibility='visible'; ;
+        }, questionAppearTime); // 1分鐘 = 60,000毫秒
+    });
+
+
+
+   
     document.querySelector('iframe').addEventListener('load', function () {
         document.getElementById('videoContainer').style.display = 'block';
     });
 
-       setTimeout(function() {
-            document.getElementById("Question").style.visibility='visible'; ;
-        }, questionAppearTime); // 1分鐘 = 60,000毫秒
 
     function submitAnswers() {
         // 取得正確的答案
@@ -30,6 +51,26 @@ questionAppearTime=5*1000;
 
        if(q1Answer.value==q1CorrectAnswer&&q2Answer.value==q2CorrectAnswer&&q3Answer.trim().toLowerCase() === q3CorrectAnswer.trim().toLowerCase()){
             alert("答對了");
+            switch (n) {
+            case 1:
+                   youtubeEmbedUrl = 'https://www.youtube.com/embed/' + questiondata[19];
+                   // 更改 <iframe> 的 src 屬性
+                   iframe.src = youtubeEmbedUrl;
+                   n=n+1;
+            break;
+
+            case 2:
+                   youtubeEmbedUrl = 'https://www.youtube.com/embed/' + questiondata[28];
+                   // 更改 <iframe> 的 src 屬性
+                   iframe.src = youtubeEmbedUrl;
+                   n=n+1;
+            break;
+
+            default:
+                alert("做完了");
+            }
+
+
         }else if(q1Answer==""){
             alert("請回答上面三個問題");
 
