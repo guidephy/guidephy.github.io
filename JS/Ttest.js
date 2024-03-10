74d7b71947ecc0fc0e1e685f1370a82ed3d383d8
guidephy <50879237+guidephy@users.noreply.github.com>
2024-03-10 23:09:02 +0800

    Add files via upload

--- JS/Ttest.js
+++ JS/Ttest.js
@@ -4,6 +4,15 @@
     var questiondata;
     var allQuestionNumber;
 
+    var numQuestions
+
+    var selectedNumbers = []; // 用於存儲選擇的數字
+
+    var correctAnswer=[];
+
+    var score=0;
+
+
 
     function handleSectionClick(section) {
     sectionTitle = section.textContent; // 獲取節標題的文字內容
@@ -28,28 +37,55 @@
 
     document.getElementById("chapterChoose").style.display="none";
 
-    var numQuestions = questiondata[1];
+    allQuestionNumber=questiondata[0];
+
+    allQuestionNumber = parseInt(allQuestionNumber);
+
+    allQuestionNumber=allQuestionNumber-1;
+
+    numQuestions = questiondata[1];
+
+
+    for (var i = 0; i <= numQuestions; i++) {
+    var randomNum;
+    do {
+        // 生成一個介於 1 到 250 之間的隨機整數
+        randomNum = Math.floor(Math.random() *allQuestionNumber) + 1;
+    } while (selectedNumbers.includes(randomNum)); // 如果已經選擇過，則重新選擇
+
+    // 將選擇的數字存入數組中
+    selectedNumbers.push(randomNum);
+    }
+
+    var answer=[];
+
+
+
 
 // 獲取題目容器元素
 var questionsContainer = document.getElementById('questions-container');
 
 // 生成題目
-for (var i = 1; i <= numQuestions; i++) {
+for (var i = 1; i <=numQuestions; i++) {
+
     // 創建問題元素
     var questionDiv = document.createElement('div');
     questionDiv.classList.add('question');
 
     // 添加標題
     var questionTitle = document.createElement('h3');
+    questionTitle.id='questionTitle'+i;
     questionTitle.textContent = '問題 ' + i;
     questionDiv.appendChild(questionTitle);
 
     // 添加圖片
     var questionImage = document.createElement('img');
     questionImage.id = 'question' + i;
-    questionImage.src = '';
+    questionImage.src = questiondata[4*(selectedNumbers[i]-1)+2];
     questionImage.alt = '問題' + i;
     questionDiv.appendChild(questionImage);
+
+    correctAnswer[i]=questiondata[4*(selectedNumbers[i]-1)+3];
 
     // 添加選項
     var radioContainer = document.createElement('div');
@@ -74,10 +110,41 @@ for (var i = 1; i <= numQuestions; i++) {
 
 }
 
-    alert(data);
-  // 隱藏章節
+
   
   })
    }
+
+function submitAnswers() {
+
+ var answers = []; // 儲存所有答案的數組
+
+// 遍歷每個問題
+for (var i = 1; i <= numQuestions; i++) {
+    // 構建查詢器字符串，動態獲取每個問題的答案元素
+    var querySelectorString = 'input[name="q' + i + '"]:checked';
+    var answer = document.querySelector(querySelectorString);
+
+    // 如果找到了答案元素，將其值添加到答案數組中
+    if (answer) {
+        answers.push(answer.value);
+        if(answer.value==correctAnswer[i]){
+            score=score+100/numQuestions;
+        }else{
+             document.getElementById('questionTitle' + i).classList.add('red-text');
+             document.getElementById('questionTitle' + i).textContent = '✗ ' + document.getElementById('questionTitle' + i).textContent+"正確答案為:"+correctAnswer[i];
+        }
+       
+    }
+}
+
+
+alert("本次作答的分數為"+score+"分!");
+
+
+
+}
+
+
 
    
