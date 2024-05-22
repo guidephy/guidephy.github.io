2b1f491c0f85cd7bd15c13b6219a33fc34965ed5
guidephy <50879237+guidephy@users.noreply.github.com>
2024-05-23 01:05:58 +0800

    Update menu.js

--- JS/menu.js
+++ JS/menu.js
@@ -1,30 +1,34 @@
-function myFunction1() {
-    document.getElementById("myDropdown1").classList.toggle("show");
-}
-
-function myFunction2() {
-    document.getElementById("myDropdown2").classList.toggle("show");
-}
-
-function myFunction3() {
-    document.getElementById("myDropdown3").classList.toggle("show");
-}
-
-function myFunction4() {
-    document.getElementById("myDropdown4").classList.toggle("show");
-}
-
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    for (var d = 0; d < dropdowns.length; d++) {
-      var openDropdown = dropdowns[d];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+(function() {
+    function toggleDropdown(dropdownId) {
+        document.getElementById(dropdownId).classList.toggle("show");
     }
-  }
-}
+
+    window.myFunction1 = function() {
+        toggleDropdown("myDropdown1");
+    };
+
+    window.myFunction2 = function() {
+        toggleDropdown("myDropdown2");
+    };
+
+    window.myFunction3 = function() {
+        toggleDropdown("myDropdown3");
+    };
+
+    window.myFunction4 = function() {
+        toggleDropdown("myDropdown4");
+    };
+
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function(e) {
+        if (!e.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            for (var d = 0; d < dropdowns.length; d++) {
+                var openDropdown = dropdowns[d];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    };
+})();
